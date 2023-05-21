852f5f7073522823df003ff6aaef69fa82ae7458
JhonnyO3 <90437643+JhonnyO3@users.noreply.github.com>
2023-05-21 12:18:45 -0300

    Add files via upload

---
+++ solution/src/main/java/mineracao/dados/solution/Feign/EnderecoFeign.java
@@ -0,0 +1,16 @@
+package mineracao.dados.solution.Feign;
+
+import mineracao.dados.solution.Model.EnderecoResponseModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(url = "https://viacep.com.br/ws/", name = "viacep")
+public interface EnderecoFeign {
+
+    @GetMapping("{cep}/json")
+    @Autowired
+    EnderecoResponseModel buscaEnderecoCep(@PathVariable("cep") String cep);
+}
